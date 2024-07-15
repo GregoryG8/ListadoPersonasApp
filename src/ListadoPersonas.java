b25a23016ef1141d8e20870a3d9a89777a03f0da
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-07-14 22:36:46 -0500

    ListadoPersonas App is done

--- src/ListadoPersonas.java
+++ src/ListadoPersonas.java
@@ -10,6 +10,11 @@ public class ListadoPersonas {
         var salir = false;
         while(!salir) {
             mostrarMenu();
+            try {
+                salir = ejecutarOperacion(consola, personas);
+            }catch (Exception e){
+                System.out.println("Error" + e.getMessage());
+            }
             System.out.println();
         }
 
@@ -23,5 +28,41 @@ public class ListadoPersonas {
                 3. Salir
                 """);
         System.out.println("Que opcion?: ");
+    }
+
+    public static boolean ejecutarOperacion(Scanner consola, List<Persona> personas) {
+        var opcion = Integer.parseInt(consola.nextLine());
+        var salir = false;
+
+        switch (opcion) {
+            case 1:
+                System.out.println("Nombre: ");
+                var nombre = consola.nextLine();
+                System.out.println("Telefono: ");
+                var telefono = consola.nextLine();
+                System.out.println("Email: ");
+                var email = consola.nextLine();
+                //Crear el objeto persona
+                var persona = new Persona(nombre, telefono, email);
+                personas.add(persona);
+                System.out.println("La lista tiene: " + personas.size() + " personas");
+                break;
+
+            case 2:
+                System.out.println("Listado de personas: ");
+                //Usando lambda y metodo de referencia
+                //personas.forEach((persona) -> System.out.println(persona));
+                personas.forEach(System.out::println);
+                break;
+
+            case 3:
+                System.out.println("Hasta luego...");
+                salir = true;
+                break;
+
+            default:
+                System.out.println("Opcion no valida " + opcion);
+        }
+        return salir;
     }
 }
