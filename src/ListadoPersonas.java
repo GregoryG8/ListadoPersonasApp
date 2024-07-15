64613b909b7a67436fd5be8aaa655b4d36555999
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-07-14 22:00:41 -0500

    feat: Create menu

--- src/ListadoPersonas.java
+++ src/ListadoPersonas.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ListadoPersonas {
     public static void main(String[] args) {
+        Scanner consola = new Scanner(System.in);
+        List<Persona> personas = new ArrayList<Persona>();
 
+        var salir = false;
+        while(!salir) {
+            mostrarMenu();
+            System.out.println();
+        }
 
+    }
+
+    public static void mostrarMenu() {
+        System.out.println("""
+                *** Listado Personas ***
+                1. Agregar
+                2. Listar
+                3. Salir
+                """);
+        System.out.println("Que opcion?: ");
     }
 }
